b59151b1197182bc2db5e51f826dec9443826bee
imayushsri <ayushsri9666@gmail.com>
2024-03-18 20:23:31 +0530

    backend setup

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://localhost:27017/"
+
+const connectToMongo = async ()=>{
+        try{
+                await mongoose.connect(mongoURI)
+                     console.log("Connected to Mongo Successfully");
+                 }
+
+        catch{
+                console.error('Error in connection');
+        }
+}
+
+module.exports = connectToMongo;
